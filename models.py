52bcdfafe78eb7eba5980ba16777514b8c1b4d1d
ABINAYA014 <abiselvarajodc@gmail.com>
2020-09-24 16:56:58 +0530

    first commit

---
+++ models.py
@@ -0,0 +1,13 @@
+from django.db import models
+class Destination:
+    v0 : str
+    v1 : str
+    v2 : str
+    v3 : str
+    v4 : str
+    v5 : str
+    t1 : str
+    t2 : str
+    t3 : str
+  
+# Create your models here.
